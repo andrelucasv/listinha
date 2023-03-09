15c0bfefd485d583d1a4833c3dd2d659f80885fe
Andre Lucas <alucas748@gmail.com>
2023-03-09 14:16:38 -0300

    Criando uma navegação modular

--- app/lib/src/app_widget.dart
+++ app/lib/src/app_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:listinha/src/home/home_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 import 'shared/themes/themes.dart';
 
@@ -9,12 +9,15 @@ class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    Modular.setInitialRoute('/home/');
+
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: darkTheme,
-      home: const HomePage(),
+      routerDelegate: Modular.routerDelegate,
+      routeInformationParser: Modular.routeInformationParser,
     );
   }
 }
