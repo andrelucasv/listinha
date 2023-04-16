8220868e743eee5949184fcba3b96b40e4652993
Andre Lucas <alucas748@gmail.com>
2023-04-16 01:42:53 -0300

    Persistência de dados da aplicação funcional

--- app/lib/src/app_module.dart
+++ app/lib/src/app_module.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/configuration/configuration_page.dart';
+import 'package:listinha/src/configuration/services/configuration_service.dart';
+import 'package:listinha/src/shared/services/realm/realm_config.dart';
 import 'package:listinha/src/shared/stores/app_store.dart';
+import 'package:realm/realm.dart';
 
 import 'home/home_module.dart';
 
@@ -8,7 +11,10 @@ class AppModule extends Module {
 
   @override
   List<Bind<Object>> get binds => [
-    Bind.singleton((i) => AppStore()),
+    Bind.instance<Realm>(Realm(config)),
+    //AutoBind.factory(ConfigurationServiceImpl.new),
+    Bind.factory(<ConfigurationService>(i) => ConfigurationServiceImpl.new),
+    Bind.singleton((i) => AppStore.new),
   ];
 
   @override
